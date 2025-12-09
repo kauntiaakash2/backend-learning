b4aff4d081f0018f6bd83a8e3c58d0a288aa23b4
Akash Agarwal <akashkauntia2006@gmail.com>
2025-12-09 14:32:07 +0000

    Day1

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Backend is working!");
+  res.send("Backend is working!🎉");
 });
 
 app.listen(3000, () => {
